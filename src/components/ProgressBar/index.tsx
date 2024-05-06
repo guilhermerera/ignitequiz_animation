02af2bb0b0e53c88f63727460f42e54a24165d78
Guilherme Rera <guilhermerera@gmail.com>
2024-05-06 13:03:36 -0300

    add: progress bar animation

--- src/components/ProgressBar/index.tsx
+++ src/components/ProgressBar/index.tsx
@@ -1,18 +1,37 @@
-import { View } from 'react-native';
+import { View } from "react-native";
 
-import { styles } from './styles';
+import { styles } from "./styles";
+import Animated, {
+	useSharedValue,
+	useAnimatedStyle,
+	withTiming
+} from "react-native-reanimated";
+import { useEffect } from "react";
 
 interface Props {
-  total: number;
-  current: number;
+	total: number;
+	current: number;
 }
 
 export function ProgressBar({ total, current }: Props) {
-  const percentage = Math.round((current / total) * 100);
+	const percentage = Math.round((current / total) * 100);
+	const progress = useSharedValue(percentage);
 
-  return (
-    <View style={styles.track}>
-      <View style={[styles.progress, { width: `${percentage}%` }]} />
-    </View>
-  );
+	useEffect(() => {
+		progress.value = withTiming(percentage, {
+			duration: 500
+		});
+	}, [percentage]);
+
+	const progressAnimationStyle = useAnimatedStyle(() => {
+		return {
+			width: `${progress.value}%`
+		};
+	});
+
+	return (
+		<View style={styles.track}>
+			<Animated.View style={[styles.progress, progressAnimationStyle]} />
+		</View>
+	);
 }
