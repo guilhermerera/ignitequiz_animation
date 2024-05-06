89b21809888ae37cddc07d9696a5cfc040a2948a
Guilherme Rera <guilhermerera@gmail.com>
2024-05-06 12:52:35 -0300

    add: color interpolation

--- src/components/Level/index.tsx
+++ src/components/Level/index.tsx
@@ -5,8 +5,10 @@ import { styles } from "./styles";
 import Animated, {
 	useSharedValue,
 	useAnimatedStyle,
-	withTiming
+	withTiming,
+	interpolateColor
 } from "react-native-reanimated";
+import { useEffect } from "react";
 
 const TYPE_COLORS = {
 	EASY: THEME.COLORS.BRAND_LIGHT,
@@ -27,12 +29,28 @@ export function Level({
 	...rest
 }: Props) {
 	const scale = useSharedValue(1);
+	const checked = useSharedValue(0);
 
 	const COLOR = TYPE_COLORS[type];
 
 	const animatedContainerStyle = useAnimatedStyle(() => {
 		return {
-			transform: [{ scale: scale.value }]
+			transform: [{ scale: scale.value }],
+			backgroundColor: interpolateColor(
+				checked.value,
+				[0, 1],
+				["transparent", COLOR]
+			)
+		};
+	});
+
+	const animatedTextStyle = useAnimatedStyle(() => {
+		return {
+			color: interpolateColor(
+				checked.value,
+				[0, 1],
+				[COLOR, THEME.COLORS.GREY_100]
+			)
 		};
 	});
 
@@ -47,24 +65,25 @@ export function Level({
 		});
 	}
 
+	useEffect(() => {
+		checked.value = withTiming(isChecked ? 1 : 0, {
+			duration: 200
+		});
+	}, [isChecked]);
+
 	return (
 		<Pressable onPressIn={onPressIn} onPressOut={onPressOut} {...rest}>
 			<Animated.View
 				style={[
 					styles.container,
-					animatedContainerStyle,
 					{
-						borderColor: COLOR,
-						backgroundColor: isChecked ? COLOR : "transparent"
-					}
+						borderColor: COLOR
+					},
+					animatedContainerStyle
 				]}>
-				<Text
-					style={[
-						styles.title,
-						{ color: isChecked ? THEME.COLORS.GREY_100 : COLOR }
-					]}>
+				<Animated.Text style={[styles.title, animatedTextStyle]}>
 					{title}
-				</Text>
+				</Animated.Text>
 			</Animated.View>
 		</Pressable>
 	);
