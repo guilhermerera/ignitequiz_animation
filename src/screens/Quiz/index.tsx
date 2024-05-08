95194ecd7b0c5528a64dafc1f8886a9882648c07
Guilherme Rera <guilhermerera@gmail.com>
2024-05-08 14:15:56 -0300

    add: quiz header animation

--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
-import { Alert, ScrollView, View } from "react-native";
+import { Alert, Text, View } from "react-native";
 import Animated, {
+	Extrapolate,
+	interpolate,
+	useAnimatedScrollHandler,
 	useAnimatedStyle,
 	useSharedValue,
 	withSequence,
@@ -19,6 +22,8 @@ import { Question } from "../../components/Question";
 import { QuizHeader } from "../../components/QuizHeader";
 import { ConfirmButton } from "../../components/ConfirmButton";
 import { OutlineButton } from "../../components/OutlineButton";
+import { ProgressBar } from "../../components/ProgressBar";
+import { THEME } from "../../styles/theme";
 
 interface Params {
 	id: string;
@@ -36,6 +41,7 @@ export function Quiz() {
 	);
 
 	const shake = useSharedValue(0);
+	const scrollY = useSharedValue(0);
 
 	const { navigate } = useNavigation();
 
@@ -125,6 +131,50 @@ export function Quiz() {
 		};
 	});
 
+	const scrolHandler = useAnimatedScrollHandler({
+		onScroll: (event) => {
+			scrollY.value = event.contentOffset.y;
+		}
+	});
+
+	const fixedProgressBarStyles = useAnimatedStyle(() => {
+		return {
+			zIndex: 1,
+			position: "absolute",
+			paddingTop: 50,
+			backgroundColor: THEME.COLORS.GREY_500,
+			width: "110%",
+			left: "-5%",
+			opacity: interpolate(scrollY.value, [50, 90], [0, 1], Extrapolate.CLAMP),
+			transform: [
+				{
+					translateY: interpolate(
+						scrollY.value,
+						[50, 100],
+						[-40, 0],
+						Extrapolate.CLAMP
+					)
+				}
+			]
+		};
+	});
+
+	const headerStyles = useAnimatedStyle(() => {
+		return {
+			opacity: interpolate(scrollY.value, [70, 90], [1, 0], Extrapolate.CLAMP),
+			transform: [
+				{
+					translateY: interpolate(
+						scrollY.value,
+						[70, 90],
+						[0, -40],
+						Extrapolate.CLAMP
+					)
+				}
+			]
+		};
+	});
+
 	useEffect(() => {
 		const quizSelected = QUIZ.filter((item) => item.id === id)[0];
 		setQuiz(quizSelected);
@@ -143,14 +193,25 @@ export function Quiz() {
 
 	return (
 		<View style={styles.container}>
-			<ScrollView
-				showsVerticalScrollIndicator={false}
-				contentContainerStyle={styles.question}>
-				<QuizHeader
-					title={quiz.title}
-					currentQuestion={currentQuestion + 1}
-					totalOfQuestions={quiz.questions.length}
+			<Animated.View style={fixedProgressBarStyles}>
+				<Text style={styles.title}>{quiz.title}</Text>
+				<ProgressBar
+					total={quiz.questions.length}
+					current={currentQuestion + 1}
 				/>
+			</Animated.View>
+			<Animated.ScrollView
+				showsVerticalScrollIndicator={false}
+				contentContainerStyle={styles.question}
+				onScroll={scrolHandler}
+				scrollEventThrottle={16}>
+				<Animated.View style={[styles.header, headerStyles]}>
+					<QuizHeader
+						title={quiz.title}
+						currentQuestion={currentQuestion + 1}
+						totalOfQuestions={quiz.questions.length}
+					/>
+				</Animated.View>
 				<Animated.View style={shakeStyleAnimation}>
 					<Question
 						key={quiz.questions[currentQuestion].title}
@@ -164,7 +225,7 @@ export function Quiz() {
 					<OutlineButton title='Parar' onPress={handleStop} />
 					<ConfirmButton onPress={handleConfirm} />
 				</View>
-			</ScrollView>
+			</Animated.ScrollView>
 		</View>
 	);
 }
